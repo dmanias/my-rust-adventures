dac2c4c7199a35591bc08854eda2ea91c5a02f86
Dimos <dimosthenis.manias@gmail.com>
2022-07-11 12:02:19 +0300

    it works

--- unique-items/src/main.rs
+++ unique-items/src/main.rs
@@ -1,19 +1,24 @@
 fn unique(a: &mut Vec<i32>)-> &mut Vec<i32>{
-    let mut index=0;
-    let mut temp;
-    let b = a.clone();
+   // let mut index=0;
+    let mut temp=-1;
+    // let b = a.clone();
+    let mut len = a.len();
 
    // let mut remove_elements = Vec::new();
-    for i in b{
-        temp = i;
-        println!("temp {:?}", temp);
-
-        for j in index +1 .. a.len(){
-            a.remove(j);
+    for i in 0 .. len {
+        if i<a.len(){
+        temp = a[i];
+        println!("temp len i {} {} {}", temp, len, i);
         }
-     
+        for j in i +1 .. len {
+            while j < a.len() && a[j] == temp {
+                    println!("a[j] len j {} {} {}", a[j], len, j);
+            a.remove(j);
+            len = len -1;              
+        }
+    }     
 
-        index = index +1;
+     //   index = index +1;
     }
 println!("{:?}", a);
     return a;
